55886aa60fed770c91b4f92215d6c8632f53beaa
Ammar Officewala <ammar.o.serpentcs@gmail.com>
2018-06-07 13:01:12 +0530

    [MIG] Migreted pos modules from 10 to 11.

---
+++ pos_backend_partner/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Akretion (http://www.akretion.com).
+# @author Raphaël Reverdy <raphael.reverdy@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "POS Backend Customer",
+    "summary": "Choose point of sale customers in backend",
+    "version": "11.0.1.0.0",
+    "category": "Point of sale",
+    "website": "http://www.akretion.com",
+    'author': "Akretion,Odoo, "
+              "Serpent Consulting Services PVT.LTD., "
+              " Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    'installable': True,
+    "depends": [
+        "pos_backend_communication",
+    ],
+    "data": [
+        'views/assets.xml',
+        'views/backend_partner.xml',
+    ],
+}
