50e68fc579b167c82f1ad02d7ff867cdec53afbd
Ammar Officewala <ammar.o.serpentcs@gmail.com>
2018-06-15 11:51:48 +0530

    [MIG] Migreted pos modules from 10 to 11.

---
+++ pos_loyalty/models/__init__.py
@@ -0,0 +1,8 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import loyalty_program
+from . import loyalty_reward
+from . import loyalty_rule
+from . import pos_config
+from . import pos_order
+from . import res_partner
