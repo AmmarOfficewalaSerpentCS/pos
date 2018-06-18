ed0a1be3cd941db5b4d0bfe25cf8f04da4af510f
Ammar Officewala <ammar.o.serpentcs@gmail.com>
2018-06-18 13:41:28 +0530

    [MIG] Migrated pos modules from 10 to 11.

---
+++ pos_default_empty_image/static/src/js/db.js
@@ -0,0 +1,18 @@
+odoo.define('pos_default_empty_image.db', function (require) {
+    "use strict";
+
+    var models = require('point_of_sale.models');
+
+    var _super_posmodel = models.PosModel.prototype;
+
+    // load new field 'has_image' for 'product.product' model
+    models.PosModel = models.PosModel.extend({
+        initialize: function (session, attributes) {
+         console.log('strettettetetteetette')
+         var product_model = _.find(this.models, function(model){ return model.model === 'product.product'; });
+            product_model.fields.push('has_image');
+            console.log('startssssssssssssssssssssssssssssss')
+            return _super_posmodel.initialize.call(this, session, attributes);
+        },
+    });
+});
